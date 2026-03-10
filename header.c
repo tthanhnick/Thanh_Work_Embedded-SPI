8b63dc77709abff631b773953537a95c35bd1e5a
Nguyen Tien Thanh <92678285+tthanhnick@users.noreply.github.com>
2026-03-10 20:04:49 +0700

    Update header.c

--- header.c
+++ header.c
@@ -32,10 +32,8 @@ void LEDConfig (void)
 	SetOrClearBit((unsigned int*) PCC_PORTD, 30u, SET_BIT);
 	SetOrClearMultiBit((unsigned int*) PORTD_PCR15, 8u, 1u, SET_BIT);
 	SetOrClearBit((unsigned int*) GPIOD_PDDR, 15u, SET_BIT); 
-	
 	SetOrClearBit((unsigned int*) GPIOD_PDOR, 15u, SET_BIT); 
 }
-
 
 void SysTick(void)
 {
@@ -61,7 +59,7 @@ void PLLConfig(void)
 
 void SoscConfig(void)
 {
-	SetOrClearBit((unsigned int*) SCG_SOSCCSR, 0, CLEAR_BIT); // disble SOSCCSR
+	SetOrClearBit((unsigned int*) SCG_SOSCCSR, 0, CLEAR_BIT); // disable SOSCCSR
 	SetOrClearBit((unsigned int*) SCG_SOSCCSR, 23, CLEAR_BIT); // Unlock SOSCCSR
 	
 	SetOrClearMultiBit((unsigned int*) SCG_SOSCDIV, 0u, 0b001 , SET_BIT); //SOSC div 1 div by 1
@@ -86,83 +84,69 @@ void ClkOutput(void)
 	
 	SetOrClearMultiBit((unsigned int*) SCG_RCCR, 16u, 0b0000 , SET_BIT); // core div by 1
 	SetOrClearMultiBit((unsigned int*) SCG_RCCR, 4u, 0b0001 , SET_BIT); // bus div by 2
-	SetOrClearMultiBit((unsigned int*) SCG_RCCR, 0u, 0b1111 , CLEAR_BIT); // divslow by 3	
+	SetOrClearMultiBit((unsigned int*) SCG_RCCR, 0u, 0b1111 , CLEAR_BIT); // divslow clear
 	SetOrClearMultiBit((unsigned int*) SCG_RCCR, 0u, 0b0010 , SET_BIT); // divslow by 3
 }
 
 void SPIConfig(void)
 {
-	SetOrClearBit((unsigned int*) PCC_PORTB, 30u, SET_BIT); // PCC PORTC CGC
+	SetOrClearBit((unsigned int*) PCC_PORTB, 30u, SET_BIT); // PCC PORTB CGC
 	SetOrClearMultiBit((unsigned int*) PORTB_PCR2, 8u, 3u , SET_BIT); // Port B2: MUX = ALT3, LPSPI0_SCK
 	SetOrClearMultiBit((unsigned int*) PORTB_PCR3, 8u, 3u , SET_BIT); // Port B3: MUX = ALT3, LPSPI0_SIN
 	SetOrClearMultiBit((unsigned int*) PORTB_PCR4, 8u, 3u , SET_BIT); // Port B4: MUX = ALT3, LPSPI0_SOUT
 	SetOrClearMultiBit((unsigned int*) PORTB_PCR5, 8u, 3u , SET_BIT); // Port B5: MUX = ALT3, LPSPI0_PCS1
 	
-	SetOrClearMultiBit((unsigned int*) PCC_LPSPI0, 24u, 1u, SET_BIT); //SPI PCC PCS
-	SetOrClearBit((unsigned int*) PCC_LPSPI0, 30u, SET_BIT); //SPI PCC CGC
-	
-	LPSPI0->TCR = 0x09000007; // Cannot use each function to turn each bit on. Cannot find issue 
-	
-//	LPSPI0->TCR |= (0b001<<27); // Prescale div by 2
-	LPSPI0->CCR |= (2u <<0); // SCK Div (Div + 2)
-	
-//	LPSPI0->TCR &= ~(0b11<<30); // CPOL, CPHA = 0
-//	
-//	LPSPI0->TCR |= (7u<<0); // 8bit = FRAMZ + 1
-//	LPSPI0->TCR &=~ (1<<23); // LSBF MSB first
-//	
-//	LPSPI0->TCR |= (0b01<<24); // PCS 1
-	
-	LPSPI0->FCR &= ~(0b11<<16); // RXWATER = 0
-	LPSPI0->FCR &= ~(0b11<<0); // TXWATER = 0
-	LPSPI0->CFGR1 &= ~(1<<3); //  NOSTALL=0 Stall if Tx FIFO empty or Rx FIFO full
+	SetOrClearMultiBit((unsigned int*) PCC_LPSPI0, 24u, 1u, SET_BIT); // SPI PCC clock source = SOSCDIV2
+	SetOrClearBit((unsigned int*) PCC_LPSPI0, 30u, SET_BIT); // SPI PCC CGC enable
 
-	LPSPI0->CFGR1 |= (1<<0); // Master mode
-	LPSPI0->CR |= (1<<3); // Debug Enable
-	
-	LPSPI0->CR |= (1<<0); // Module Enable
+	LPSPI0->TCR = 0x0900000F; // [29:27]=001 PRESCALE/2, [25:24]=01 PCS1, [3:0]=0xF FRAMESZ=16bit
+
+	LPSPI0->CCR |= (2u << 0); // SCK Div: SCK = SOSCDIV2 / (SCKDIV+2) = 8MHz / 4 = 2MHz
+
+	LPSPI0->FCR  &= ~(0b11 << 16); // RXWATER = 0
+	LPSPI0->FCR  &= ~(0b11 << 0);  // TXWATER = 0
+	LPSPI0->CFGR1 &= ~(1 << 3);    // NOSTALL=0: stall if TX FIFO empty or RX FIFO full
+
+	LPSPI0->CFGR1 |= (1 << 0); // Master mode
+	LPSPI0->CR    |= (1 << 3); // Debug Enable
+	LPSPI0->CR    |= (1 << 0); // Module Enable
 }
 
 void SPISendChar(char c)
 {
-	if ((LPSPI0->SR) & (1<<0)) //Tx FIFO available 
-	{
-		LPSPI0->TDR = c; // send data
-		LPSPI0->SR |= (1<<0); //write 1 to clear
-	}
+	while (!((LPSPI0->SR) & (1 << 0))); // Wait until TX FIFO has space (TDF=1)
+	LPSPI0->TDR = (unsigned char)c;     // Send data
 }
 
 void SPISendString (char data_string[])
 {
 	int i = 0;
-	while (data_string [i] != '\0')
+	while (data_string[i] != '\0')
 	{
-		SPISendChar(data_string [i]);
+		SPISendChar(data_string[i]);
 		i++;
 	}
 }
 
 void SPISendLED(unsigned int address, unsigned int data)
 {
-	if ((LPSPI0->SR) & (1<<0)) //Tx FIFO available 
-	{
-		LPSPI0->TDR = address; // send data 'address'
-		LPSPI0->SR |= (1<<0); //write 1 to clear
-	}
-	
-	if ((LPSPI0->SR) & (1<<0)) //Tx FIFO available 
-	{
-		LPSPI0->TDR = data; // send data 'data'
-		LPSPI0->SR |= (1<<0); //write 1 to clear
-	}
+	while (!((LPSPI0->SR) & (1 << 0))); // Wait until TX FIFO has space (TDF=1, read-only)
+	LPSPI0->TDR = ((address & 0xFFu) << 8) | (data & 0xFFu); // Send 16-bit: MSB=address, LSB=data
+
+	while (!(LPSPI0->SR & (1 << 9)));  // Wait for Frame Complete Flag (FCF)
+	LPSPI0->SR |= (1 << 9);            // Clear FCF (W1C)
+}
+
+void MAX7219Init(void)
+{
+	SPISendLED(decode,       0x00u); // No-decode mode for all digits
+	SPISendLED(intensity,    0x0Au); // Brightness ~2/3
+	SPISendLED(scan_limit,   0x07u); // Scan all 8 digits
+	SPISendLED(shut_down,    0x01u); // Normal operation (exit shutdown)
+	SPISendLED(display_test, 0x00u); // Display test off
 }
 
 void DisplayLetterLED(void)
 {
-	SPISendLED(decode, 0x00u);
-	SPISendLED(intensity, 0x0Au);
-	SPISendLED(scan_limit, 0x07u);
-	SPISendLED(shut_down, 0x01u);
-	SPISendLED(display_test, 0x00u);
-	SPISendLED(first_digit, 0x67u); // send 'H'
+	SPISendLED(first_digit, SEG_H); // Display 'H' on digit 1
 }
